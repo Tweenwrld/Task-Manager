4e90733fdc4cac857db248668eab4224c3b0caa0
Tweenwrld <154606522+Tweenwrld@users.noreply.github.com>
2025-06-25 19:51:02 +0300

    initial commit.

---
+++ eslint.config.js
@@ -0,0 +1,45 @@
+import js from '@eslint/js';
+import globals from 'globals';
+import react from 'eslint-plugin-react';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+
+export default [
+  {
+    files: ['**/*.{js,jsx}'],
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+      globals: {
+        ...globals.browser,
+        ...globals.es2021,
+      },
+      parserOptions: {
+        ecmaFeatures: { jsx: true },
+      },
+    },
+    plugins: {
+      react,
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
+    },
+    rules: {
+      ...js.configs.recommended.rules,
+      ...react.configs.recommended.rules,
+      ...reactHooks.configs.recommended.rules,
+      'react-refresh/only-export-components': [
+        'warn',
+        { allowConstantExport: true },
+      ],
+      'react/prop-types': 'off', // Disable prop-types linting (using prop-types package)
+      'no-unused-vars': ['warn'],
+      'react/jsx-uses-react': 'off', // Not needed in React 19
+      'react/jsx-uses-vars': 'error',
+    },
+    settings: {
+      react: {
+        version: '19.1', // Specify React 19
+      },
+    },
+  },
+];
